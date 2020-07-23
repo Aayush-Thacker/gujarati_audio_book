dc5824636f6060f61dfc0c3e9630661559be0156
Aayush Thacker <aayushthacker24@gmail.com>
2020-07-23 14:57:17 +0530

    Pages added, 6 books in each page

--- application.py
+++ application.py
@@ -21,9 +21,27 @@ def serve_static(filename):
 
 @app.route("/")
 def index():
+    if session.get("page") == None:
+        session["page"] = 1
+
     with open('books.json','r') as json_file:
         books = json.load(json_file)
-    return render_template("home.html", data = books)
+    
+    if session["page"] == 1:
+        data = books["books"]
+    else:
+        data = books[f"book"+str(session["page"])]
+    
+    lp = False
+    if len(data)!=6:
+        lp = True
+
+    return render_template("home.html", data = data, page = session["page"], search = False, l = lp)
+
+@app.route("/page/<int:p>")
+def page(p):
+    session["page"] = p
+    return redirect(url_for("index"))
 
 @app.route("/search", methods=['POST'])
 def search():
@@ -34,15 +52,16 @@ def search():
     data = {"books": []}
     count = 0
 
-    for book in books['books']:
-        if item in book['name'] or item in book['author']:
-            data["books"].append(book)
-            count+=1
+    for b in books:
+        for book in books[b]:
+            if item in book['name'] or item in book['author']:
+                data["books"].append(book)
+                count+=1
 
     if(count>0):
-        return render_template("home.html", data = data)
+        return render_template("home.html", data = data["books"], search = True)
     else:
-        return render_template("home.html", data = None)
+        return render_template("home.html", data = None, search = True)
 
 @app.route("/user", methods = ['GET', 'POST'])
 def user():
@@ -101,12 +120,18 @@ def secret():
         f3 = request.files['pdf']
         f3.save(os.path.join(app.config["PDF_UPLOAD"], f3.filename))
 
-        with open('books.json','r', encoding='utf-8') as json_file:
+        with open('books.json','r+', encoding='utf-8') as json_file:
             books = json.load(json_file)
 
-        books['books'].append({"name" : name,"author" : author,"details" : details,"image" : f1.filename,"audio" : audios,"pdf" : f3.filename})
-        
-        with open('books.json','w') as json_file:
+            count = 1
+            for book in books:
+                if len(books[book]) < 6:
+                    books[book].append({"name" : name,"author" : author,"details" : details,"image" : f1.filename,"audio" : audios,"pdf" : f3.filename})
+                count+=1
+            else:
+                d = {f"book{count}":[{"name" : name,"author" : author,"details" : details,"image" : f1.filename,"audio" : audios,"pdf" : f3.filename}]}
+                books.update(d)
+            json_file.seek(0)
             json.dump(books, json_file)
 
         return render_template("upload.html", message="Y")
@@ -123,9 +148,10 @@ def book():
     with open('books.json','r') as f:
         books = json.load(f)
     
-    for book in books['books']:
-        if book['name'] == session.get("bname"):
-            return render_template("book.html", data = book)
+    for b in books:
+        for book in books[b]:
+            if book['name'] == session.get("bname"):
+                return render_template("book.html", data = book)
     else:
         return render_template("book.html", data = None)
 
